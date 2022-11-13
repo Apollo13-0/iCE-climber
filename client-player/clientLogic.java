fd44c925ae43ede653cb959823bed50eb306620c
Apollo13-0 <calderon.ig08@estudiantec.cr>
2022-11-12 19:15:36 -0600

    add client logic

--- client-player/clientLogic.java
+++ client-player/clientLogic.java
@@ -1,42 +1,129 @@
-import java.net.*;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
+/**
+ * This class handles the client connection.
+ */
 public class clientLogic {
+    /**
+     *  This is the port number, which begins at 1024.
+     */
+    private int port;
+
+    /**
+     * String containing the IP of the computer that the client is going connect.
+     */
+    private String IPserver;
+
+    /**
+     * This is the socket that is used to transfer information between computers.
+     */
     private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
 
-    public clientLogic(){}
+    /**
+     * This lets the aplication read primitive Java data types.
+     */
+    private DataInputStream inputSocketInD;
 
-    public void startConnection(String ip, int port) {
-        try{
-            clientSocket = new Socket(ip, port);
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+    /**
+     * This lets tje application write primitive Java data type.
+     */
+    private DataOutputStream clientOutD;
 
-    }
-
-    public void sendMessage(String msg) {
-        try{
-            out.println(msg);
-            String resp = in.readLine();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-    }
-
-    public void stopConnection() {
-
+    /**
+     * This is the constructor, it finds the server and connects to it.
+     *
+     * @param newPort int.
+     * @param newIPserver String.
+     */
+    public clientLogic(int newPort, String newIPserver) {
+        this.port = newPort;
+        this.IPserver = newIPserver;
         try {
-            in.close();
-            out.close();
-            clientSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            this.clientSocket = new Socket(this.IPserver, this.port);
+            this.inputSocketInD = new DataInputStream(this.clientSocket.getInputStream());
+            this.clientOutD = new DataOutputStream(this.clientSocket.getOutputStream());
+        } catch (UnknownHostException e) {
+            closeConexion();
+        } catch (Exception e) {
+            closeConexion();
+        }
+    }
+
+
+    /**
+     * This method returns the port number.
+     *
+     * @return Value of port.
+     */
+    public int getPort() {
+        return this.port;
+    }
+
+    /**
+     * This method returns the IP of the server.
+     *
+     * @return value of IPserver.
+     */
+    public String getIPserver() {
+        return this.IPserver;
+    }
+
+    /**
+     * This method connect to the server and send the jackson.
+     *
+     * @param jacksonStr New jackson String.
+     */
+    public void writeSocket(String jacksonStr) {
+        try {
+            this.clientOutD.writeUTF(jacksonStr);
+            this.clientSocket.setKeepAlive(true);
+        } catch (IOException ioException) {
+            closeConexion();
+        }
+    }
+
+    /**
+     * This method waits for the server and if there is one server it read the information and update the
+     * information.
+
+    public void readSockect() {
+        ObjectMapper mapper = new ObjectMapper();
+        Thread thread = new Thread(() -> {
+            try {
+                String message = this.inputSocketInD.readUTF();
+
+                UpdateInfo Info = mapper.readValue(message, UpdateInfo.class);
+
+                Game.getInstance().setUpdateInfo(Info);
+
+                this.clientSocket.setKeepAlive(true);
+            } catch (JsonMappingException e) {
+                e.printStackTrace();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } catch (SocketException e) {
+                closeConexion();
+            } catch (IOException e) {
+                closeConexion();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+     */
+
+    /**
+     * This method closes the connection between the server and the client.
+     */
+    public void closeConexion(){
+        try {
+            this.clientSocket.close();
+        } catch (IOException ignored) {
         }
     }
 }
