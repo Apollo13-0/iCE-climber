1796db6d009e81e27b5f9e684b40fb79b6ff7f80
Apollo13-0 <calderon.ig08@estudiantec.cr>
2022-11-17 19:26:10 -0600

    updateInfo class

---
+++ client-player/updateInfo.java
@@ -0,0 +1,266 @@
+import game_interface.Jugador;
+import game_interface.Sprite;
+
+public class updateInfo {
+
+    /**
+     * Identifier of the player
+     */
+    private int ID;
+
+    /**
+     * Coords in X of player 1
+     */
+    private int player1X;
+
+    /**
+     * Coords in Y of player 1
+     */
+    private int player1Y;
+
+    /**
+     * Coords in X of player 1
+     */
+    private int player2X;
+
+    /**
+     * Coords in Y of player 1
+     */
+    private int player2Y;
+
+    /**
+     * life of player 1
+     */
+    private int lifePlayer1;
+
+    /**
+     * life of player 2
+     */
+    private int lifePlayer2;
+
+    /**
+     *  score of player 1
+     */
+    private int scorePlayer1;
+
+    /**
+     *  score of player 2
+     */
+    private int scorePlayer2;
+
+    /**
+     *  all the sprites on the game
+     */
+    private Sprite[] spritesList;
+
+    /**
+     *  all the blocks on the game
+     */
+    //private Bloque[] bloqueList;
+
+    /**
+     *  all the bonus on the game
+     */
+    //privete verduras[] verdurasList;
+
+    /**
+     *  indicates if the game is in the bonus phase
+     */
+    private boolean bonusPhase;
+
+    public updateInfo(){ }
+
+    public updateInfo(int ID, int player1X, int player1Y, int player2X, int player2Y, int lifePlayer1, int lifePlayer2, int scorePlayer1, int scorePlayer2, Sprite[] spritesList, boolean bonusPhase) {
+        this.ID = ID;
+        this.player1X = player1X;
+        this.player1Y = player1Y;
+        this.player2X = player2X;
+        this.player2Y = player2Y;
+        this.lifePlayer1 = lifePlayer1;
+        this.lifePlayer2 = lifePlayer2;
+        this.scorePlayer1 = scorePlayer1;
+        this.scorePlayer2 = scorePlayer2;
+        this.spritesList = spritesList;
+        this.bonusPhase = bonusPhase;
+    }
+
+    /**
+     * Gets life of player 2.
+     *
+     * @return Value of life of player 2.
+     */
+    public int getLifePlayer2() {
+        return lifePlayer2;
+    }
+
+    /**
+     * Gets Coords in X of player 1.
+     *
+     * @return Value of Coords in X of player 1.
+     */
+    public int getPlayer2X() {
+        return player2X;
+    }
+
+    /**
+     * Sets new Coords in Y of player 1.
+     *
+     * @param player2Y New value of Coords in Y of player 1.
+     */
+    public void setPlayer2Y(int player2Y) {
+        this.player2Y = player2Y;
+    }
+
+    /**
+     * Sets new indicates if the game is in the bonus phase.
+     *
+     * @param bonusPhase New value of indicates if the game is in the bonus phase.
+     */
+    public void setBonusPhase(boolean bonusPhase) {
+        this.bonusPhase = bonusPhase;
+    }
+
+    /**
+     * Gets Coords in Y of player 1.
+     *
+     * @return Value of Coords in Y of player 1.
+     */
+    public int getPlayer2Y() {
+        return player2Y;
+    }
+
+    /**
+     * Sets new Coords in Y of player 1.
+     *
+     * @param player1Y New value of Coords in Y of player 1.
+     */
+    public void setPlayer1Y(int player1Y) {
+        this.player1Y = player1Y;
+    }
+
+    /**
+     * Gets Coords in Y of player 1.
+     *
+     * @return Value of Coords in Y of player 1.
+     */
+    public int getPlayer1Y() {
+        return player1Y;
+    }
+
+    /**
+     * Sets new score of player 1.
+     *
+     * @param scorePlayer1 New value of score of player 1.
+     */
+    public void setScorePlayer1(int scorePlayer1) {
+        this.scorePlayer1 = scorePlayer1;
+    }
+
+    /**
+     * Gets indicates if the game is in the bonus phase.
+     *
+     * @return Value of indicates if the game is in the bonus phase.
+     */
+    public boolean isBonusPhase() {
+        return bonusPhase;
+    }
+
+    /**
+     * Sets new life of player 2.
+     *
+     * @param lifePlayer2 New value of life of player 2.
+     */
+    public void setLifePlayer2(int lifePlayer2) {
+        this.lifePlayer2 = lifePlayer2;
+    }
+
+    /**
+     * Sets new Coords in X of player 1.
+     *
+     * @param player1X New value of Coords in X of player 1.
+     */
+    public void setPlayer1X(int player1X) {
+        this.player1X = player1X;
+    }
+
+    /**
+     * Gets score of player 1.
+     *
+     * @return Value of score of player 1.
+     */
+    public int getScorePlayer1() {
+        return scorePlayer1;
+    }
+
+    /**
+     * Sets new score of player 2.
+     *
+     * @param scorePlayer2 New value of score of player 2.
+     */
+    public void setScorePlayer2(int scorePlayer2) {
+        this.scorePlayer2 = scorePlayer2;
+    }
+
+    /**
+     * Gets Identifier of the player.
+     *
+     * @return Value of Identifier of the player.
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * Sets new life of player 1.
+     *
+     * @param lifePlayer1 New value of life of player 1.
+     */
+    public void setLifePlayer1(int lifePlayer1) {
+        this.lifePlayer1 = lifePlayer1;
+    }
+
+    /**
+     * Gets score of player 2.
+     *
+     * @return Value of score of player 2.
+     */
+    public int getScorePlayer2() {
+        return scorePlayer2;
+    }
+
+    /**
+     * Sets new Coords in X of player 1.
+     *
+     * @param player2X New value of Coords in X of player 1.
+     */
+    public void setPlayer2X(int player2X) {
+        this.player2X = player2X;
+    }
+
+    /**
+     * Gets Coords in X of player 1.
+     *
+     * @return Value of Coords in X of player 1.
+     */
+    public int getPlayer1X() {
+        return player1X;
+    }
+
+    /**
+     * Sets new Identifier of the player.
+     *
+     * @param ID New value of Identifier of the player.
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * Gets life of player 1.
+     *
+     * @return Value of life of player 1.
+     */
+    public int getLifePlayer1() {
+        return lifePlayer1;
+    }
+}
